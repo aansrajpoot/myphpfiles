8d0f976fc872c6503644bedc297cc2d7f96f14e4
aansrajpoot <76785331+aansrajpoot@users.noreply.github.com>
2021-08-03 18:20:39 +0500

    Add files via upload

---
+++ SukkurIBAFacultyPage.php
@@ -0,0 +1,291 @@
+<!doctype html>
+<html lang="en">
+  <head>
+
+    
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <style>
+        ul li{
+            list-style-type: none;
+        }
+        ul li::before {
+  content: "\2022";
+  color: grey;
+  font-weight: bold;
+  display: inline-block; 
+  width: 1em;
+  margin-left: -2em;
+  
+}
+    </style>
+  </head>
+  <body>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+    <div class="container">
+        <div class="row">
+            
+            <div class="col-sm-8 mt-4">
+                <nav aria-label="breadcrumb" >
+                    <ol class="breadcrumb">
+                      <li class="breadcrumb-item"><a href="#" style="text-decoration: none;">Home</a></li>
+                      <li class="breadcrumb-item"><a href="#" style="text-decoration: none;">Faculty</a></li>
+                      <li class="breadcrumb-item active" aria-current="page">Computer Science</li>
+                    </ol>
+                  </nav>
+                  <h2 >Computer Science
+                    <hr style="width: 30px; ">
+                  </h2>
+                <div class="row">
+
+                    <div class="col-sm-4">
+                        <div class="card" style="border: none;">
+                            <img src="https://facultyimages.iba-suk.edu.pk/ADM-0089.jpg" alt="..." style="height: 280px;">
+                            <div class="card-body">
+                              <h6 class="card-title"><a href="" style="text-decoration: none;">Zahid Hussain Khand </a></h6>
+                              <p class="card-text">Registrar (BPS-21)<hr></p>
+                              <span style="font-size: 9.5pt;">
+                                <i class="fas fa-envelope-open"></i>
+                              zahid@iba-suk.edu.pk
+                            </span><br><br>
+                              <a href="#" class="btn btn-light border border danger"> View Details</a>
+                            </div>
+                          </div>
+                    </div>
+
+                    <div class="col-sm-4">
+                        <div class="card" style="border: none;">
+                            <img src="https://facultyimages.iba-suk.edu.pk/INS-0050.jpg"  class="img-fluid" alt="..."  >
+                            <div class="card-body" >
+                              <h6 class="card-title"><a href="" style="text-decoration: none;">Ahmed Waqas </a></h6>
+                              <p class="card-text">HOD (BPS) <hr></p>
+                              <span style="font-size: 9.5pt;">
+                                <i class="fas fa-envelope-open"></i>
+                              waqas@iba-suk.edu.pk
+                            </span><br><br>
+                              <a href="#" class="btn btn-light border">View Details</a>
+                            </div>
+                          </div>
+                    </div>
+
+                    <div class="col-sm-4">
+                        <div class="card" style="border: none;">
+                            <img src="https://facultyimages.iba-suk.edu.pk/INS-0150.jpg" class="img-fluid" alt="...">
+                            <div class="card-body">
+                              <h6 class="card-title"><a href="" style="text-decoration: none;">M. Abdul Rehman Soomrani</a></h6>
+                              <p class="card-text">Asistant professor (BPS)<hr></p>
+                              <span style="font-size: 9.5pt;">
+                                <i class="fas fa-envelope-open"></i>
+                              somrani@iba-suk.edu.pk
+                            </span><br><br>
+                              <a href="#" class="btn btn-light border">View Details</a>
+                            </div>
+                          </div>
+                    </div>
+
+                    <div class="col-sm-4 mt-2">
+                        <div class="card" style="border: none;">
+                            <img src="https://facultyimages.iba-suk.edu.pk/INS-0414.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                              <h4 class="card-title"><a href="" style="text-decoration: none;">Sajid khan </a></h4>
+                              <p class="card-text">Asistant professor (BPS)<hr></p>
+                              <span style="font-size: 9.5pt;">
+                                <i class="fas fa-envelope-open"></i>
+                              sajjid@iba-suk.edu.pk
+                            </span><br><br>
+                              <a href="#" class="btn btn-light border">View Details</a>
+                            </div>
+                          </div>
+                    </div>
+
+                    <div class="col-sm-4 mt-2">
+                        <div class="card" style="border: none;">
+                            <img src="https://facultyimages.iba-suk.edu.pk/INS-0019.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                              <h4 class="card-title"><a href="" style="text-decoration: none;">Dr. sheer </a></h4>
+                              <p class="card-text">Asistant professor (BPS)<hr></p>
+                              <span style="font-size: 9.5pt;">
+                                <i class="fas fa-envelope-open"></i>
+                              sheer@iba-suk.edu.pk
+                            </span><br><br>
+                              <a href="#" class="btn btn-light border">View Details</a>
+                            </div>
+                          </div>
+                    </div>
+
+                    <div class="col-sm-4 mt-2">
+                        <div class="card" style="border: none;">
+                            <img src="https://facultyimages.iba-suk.edu.pk/INS-0012.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                              <h4 class="card-title"><a href="" style="text-decoration: none;">Dr. Javed shani </a></h4>
+                              <p class="card-text">Asistant professor (BPS)<hr></p>
+                              <span style="font-size: 9.5pt;">
+                                <i class="fas fa-envelope-open"></i>
+                              javed@iba-suk.edu.pk
+                            </span><br><br>
+                              <a href="#" class="btn btn-light border">View Details</a>
+                            </div>
+                          </div>
+                    </div>
+
+                    <div class="col-sm-4 mt-2">
+                        <div class="card" style="border: none;">
+                            <img src="https://facultyimages.iba-suk.edu.pk/INS-0352.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                              <h4 class="card-title"><a href="" style="text-decoration: none;">M. Ajmal Sawand </a></h4>
+                              <p class="card-text">Asistant professor (BPS)<hr></p>
+                              <span style="font-size: 9.5pt;">
+                                <i class="fas fa-envelope-open"></i>
+                              ajmal@iba-suk.edu.pk
+                            </span><br><br>
+                              <a href="#" class="btn btn-light border">View Details</a>
+                            </div>
+                          </div>
+                    </div>
+
+                    <div class="col-sm-4 mt-2">
+                        <div class="card" style="border: none;">
+                            <img src="https://facultyimages.iba-suk.edu.pk/INS-0002.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                              <h4 class="card-title"><a href="" style="text-decoration: none;">Qamar din Khand </a></h4>
+                              <p class="card-text">Asistant professor (BPS)<hr></p>
+                              <span style="font-size: 9.5pt;">
+                                <i class="fas fa-envelope-open"></i>
+                              qamar@iba-suk.edu.pk
+                            </span><br><br>
+                              <a href="#" class="btn btn-light border">View Details</a>
+                            </div>
+                          </div>
+                    </div>
+
+                    <div class="col-sm-4 mt-2">
+                        <div class="card" style="border: none;">
+                            <img src="https://facultyimages.iba-suk.edu.pk/INS-0515.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                              <h4 class="card-title"><a href="" style="text-decoration: none;">Khalid Hussain </a></h4>
+                              <p class="card-text">Lecturer (BPS)<hr></p>
+                              <span style="font-size: 9.5pt;">
+                                <i class="fas fa-envelope-open"></i>
+                              khalid@iba-suk.edu.pk
+                            </span><br><br>
+                              <a href="#" class="btn btn-light border">View Details</a>
+                            </div>
+                          </div>
+                    </div>
+
+                    <div class="col-sm-4 mt-2">
+                        <div class="card" style="border: none;">
+                            <img src="asif.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                              <h4 class="card-title"><a href="" style="text-decoration: none;">Asif Raza </a></h4>
+                              <p class="card-text">Coordinator (BPS)<hr></p>
+                              <span style="font-size: 9.5pt;">
+                                <i class="fas fa-envelope-open"></i>
+                              asif@iba-suk.edu.pk
+                            </span><br><br>
+                              <a href="#" class="btn btn-light border">View Details</a>
+                            </div>
+                          </div>
+                    </div>
+
+                    <div class="col-sm-4 mt-2">
+                        <div class="card" style="border: none;">
+                            <img src="gm.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                              <h4 class="card-title"><a href="" style="text-decoration: none;">Ghulam Mujtaba </a></h4>
+                              <p class="card-text">Asistant professor (BPS)<hr></p>
+                              <span style="font-size: 9.5pt;">
+                                <i class="fas fa-envelope-open"></i>
+                              gm@iba-suk.edu.pk
+                            </span><br><br>
+                              <a href="#" class="btn btn-light border">View Details</a>
+                            </div>
+                          </div>
+                    </div>
+
+                    
+                    <div class="col-sm-4 mt-2">
+                      <div class="card" style="border: none;">
+                          <img src="sana.jpg" class="card-img-top" alt="...">
+                          <div class="card-body">
+                            <h4 class="card-title"><a href="" style="text-decoration: none;">Sana Fatima </a></h4>
+                            <p class="card-text">Lecturer (BPS)<hr></p>
+                            <span style="font-size: 9.5pt;">
+                              <i class="fas fa-envelope-open"></i>
+                            sana@iba-suk.edu.pk
+                          </span><br><br>
+                            <a href="#" class="btn btn-light border">View Details</a>
+                          </div>
+                        </div>
+                  </div>
+                </div>
+            </div>
+
+            <!--SEcond column for list of departments-->
+
+            <div class="col-sm-4 " style="padding: 90px;">
+
+                <h4>Faculty
+                    <hr style="width: 30px;">
+                </h4>
+                <ul>
+                    <li>App phds</li>
+                    <li>Management science</li>
+                    <li>Computer Science</li>
+                    <li>Electrical engieering</li>
+                    <li>computer system engineering</li>
+                    <li>Education</li>
+                    <li>Mathematics</li>
+                    <li>support Faculty</li>
+                </ul>
+<br><br>
+                <h4>Our professors<hr>
+                </h4>
+                <div class="card mb-3" style="max-width: 840px; height: 160px;">
+                    <div class="row no-gutters">
+                      <div class="col-md-4">
+                        <img src="https://facultyimages.iba-suk.edu.pk/INS-0004.jpg" class="card-img" alt="..." style="padding-top: 30px; padding-left: 5px;">
+                    
+                        
+                      </div>
+                      <div class="col-md-8">
+                        <div class="card-body">
+                          <h6 style="font-size: smaller;">Niaz Ahmed Bhutto
+                        <hr style="width: 30px;">
+                    </h6>
+                          <p class="card-text" style="font-size: smaller;">Hod. Bussiness Administration</p>
+                          <p class="card-text" style="font-size: smaller;" >Show Profile</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="card mb-3" style="max-width: 840px; height: 160px;">
+                    <div class="row no-gutters">
+                      <div class="col-md-4">
+                        <img src="https://facultyimages.iba-suk.edu.pk/INS-0002.jpg" class="card-img" alt="..." style="padding-top: 30px; padding-left: 5px;">
+                    
+                        
+                      </div>
+                      <div class="col-md-8">
+                        <div class="card-body">
+                          <h6 style="font-size: smaller;">Dr.Qamar din khand
+                        <hr style="width: 30px;">
+                    </h6>
+                          <p class="card-text" style="font-size: smaller;">Associate Professor</p>
+                          <p class="card-text" style="font-size: smaller;" >Show Profile</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <H4><a href="" style="text-decoration: none;">All Phds</a></H4>     
+            </div>
+            </div>
+    </div>
+    </div>
+    <h6>Created by Aans Javed</h6>
+  </body>
+</html>
